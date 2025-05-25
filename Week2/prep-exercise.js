b779a24221bdf2d66dcae7b2a9954b25b1175479
Ahmad Zetouny <Zet_Ahmad@hotmail.com>
2025-05-25 11:48:03 +0200

    Week 2 Prep-Exercise Completed

--- Week2/prep-exercise.js
+++ Week2/prep-exercise.js
@@ -13,37 +13,58 @@ try {
   await connection.query("USE food_recipes");
 
   await connection.query(`CREATE TABLE recipes (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    cooking_time INT
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
   )`);
 
   await connection.query(`CREATE TABLE ingredients (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
   )`);
 
   await connection.query(`CREATE TABLE steps (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  instructions VARCHAR(100)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    instruction VARCHAR(100) NOT NULL
   )`);
 
   await connection.query(`CREATE TABLE categories (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
   )`);
 
-  const [invitee_rows] = await connection.query(`SHOW TABLES`);
-  // console.log("\n Invitee Table:");
-  console.table(invitee_rows);
+  await connection.query(`CREATE TABLE recipe_ingredients (
+    recipe_id INT NOT NULL,
+    ingredient_id INT NOT NULL,
+    PRIMARY KEY (recipe_id, ingredient_id),
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
+    FOREIGN KEY (ingredient_id) REFERENCES ingredients(id) ON DELETE CASCADE
+  )`);
+
+  await connection.query(`CREATE TABLE recipe_steps (
+    recipe_id INT NOT NULL,
+    steps_id INT NOT NULL,
+    PRIMARY KEY (recipe_id, steps_id),
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
+    FOREIGN KEY (steps_id) REFERENCES steps(id) ON DELETE CASCADE
+  )`);
+
+  await connection.query(`CREATE TABLE recipe_categories (
+    recipe_id INT NOT NULL,
+    category_id INT NOT NULL,
+    PRIMARY KEY (recipe_id, category_id),
+    FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
+  )`);
+
+  const [tables] = await connection.query(`SHOW TABLES`);
+  console.log("\n Food Recipes Table:");
+  console.table(tables);
 } catch (error) {
   console.log(error);
 }
 
 try {
-  const [results, fields] = await connection.query(
-    `drop database food_recipes`
-  );
+  await connection.query(`drop database food_recipes`);
 } catch (error) {
   console.log(error);
 }
